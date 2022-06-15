3fbc9524ec5a8a02643d3a13992ff511747963cc
Petro Bondar <bondar.petro@lll.kpi.ua>
2022-06-15 22:41:57 +0300

    Made contains a method, fixed few minor bugs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,12 +6,24 @@ enum Set[+A]:
   case Empty
   case NonEmpty private[Set] (a: A, rest: Set[A])
 
+  def contains[A](a: A): Boolean = 
+    @tailrec
+    def cont_rec[A](set: Set[A], x: A): Boolean = 
+      set match
+        case Empty => false
+        case NonEmpty(b, tail) => 
+          if (a == b) then 
+            true 
+          else 
+            cont_rec(tail, a)
+
+    cont_rec(this, a)
 
 object Set:
   def makeSet[A](xs: A*): Set[A] = ???
     
   def add[A](st: Set[A], a: A): Set[A] = 
-    if contains(st, a) then 
+    if st.contains(a) then 
       st
     else
       NonEmpty(a, st)
@@ -20,10 +32,10 @@ object Set:
   def intersect[A](left: Set[A], right: Set[A]): Set[A] = 
     @tailrec
     def rec[A](l: Set[A], r: Set[A],
-      result: Set[A]): Set[A] = left match
+      result: Set[A]): Set[A] = l match
         case Empty => result
         case NonEmpty(x, tail) =>
-          if contains(r, x) then
+          if r.contains(x) then
             rec(tail, r, NonEmpty(x, result))
           else
             rec(tail, r, result)
@@ -38,24 +50,13 @@ object Set:
       case NonEmpty(b, tail) => union(tail, add(right, b))
 
 
-  @tailrec
-  def contains[A](s: Set[A], a: A): Boolean = 
-    s match
-      case Empty => false
-      case NonEmpty(b, set) => 
-        if (a == b) then 
-          true 
-        else 
-          contains(set, a)
-
-
   def map[A, B](base: Set[A], f: A => B): Set[B] =
     @tailrec
     def rec[A, B](bs: Set[A], 
       f: A => B, result: Set[B]): Set[B] = 
         bs match
           case Empty => result
-          case NonEmpty(x, tail) => rec(tail, f, add(result, x))
+          case NonEmpty(x, tail) => rec(tail, f, add(result, f(x)))
 
     rec(base, f, Empty)
 
