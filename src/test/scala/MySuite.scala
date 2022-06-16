ff417f0141b667ea53c5adb67ad86f3a7a5ba348
Petro Bondar <bondar.petro@lll.kpi.ua>
2022-06-16 18:18:34 +0300

    Tests for add, contains, makeSet(apply)

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -5,12 +5,50 @@ import org.scalacheck.Prop._
 import org.scalacheck.Arbitrary.arbitrary
 
 import petro.b.lab1._
-
+import scala.util.Random
 
 class SetSuite extends ScalaCheckSuite {
 
   given [A: Arbitrary]: Arbitrary[Set[A]] =
     Arbitrary(Gen.listOf(arbitrary[A]).map(Set(_*)))
+
+  property("set constructed with sequence contains it's random element, if Seq is empty, Set == Empty") {
+    forAll{
+      (xs: Seq[Int]) =>
+        if xs.length > 0 then
+          Set(xs*).contains(Random.shuffle(xs).head)
+        else
+          Set(xs*) == Set.Empty
+    }
+  }
+
+
+  property("after adding element that already in set, it's size shouldn't increase") {
+    forAll{
+      (set: Set[Int], x: Int) =>
+        if(set.contains(x))
+          set.size() == Set.add(set, x).size()
+        else
+          set.size() == Set.add(set, x).size() - 1
+    }
+  }
+
+  
+  property("set should contain object that was added") {
+    forAll {
+      (set: Set[Int], x: Int) =>
+        Set.add(set, x).contains(x)
+    }
+  }
+
+
+  property("set is it's own subset"){
+    forAll {
+      (set: Set[Int]) =>
+        Set.is_subset(set, set)
+    }
+  }
+
 
   property("intersection should return set that smaller than parameters") {
     forAll { 
